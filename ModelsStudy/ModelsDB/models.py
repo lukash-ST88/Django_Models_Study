ec983d4f78d37decfaa641c9cea91ef033a60df4
lukash-ST88 <lukash.andrei2012@yandex.ru>
2022-10-27 16:44:36 +0300

    Introduction in models is studied

---
+++ ModelsStudy/ModelsDB/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+
+class CommonInfo(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+
+    class Meta:
+        abstract = True
+
+class Student(CommonInfo):
+    home_group = models.CharField(max_length=5)
+
